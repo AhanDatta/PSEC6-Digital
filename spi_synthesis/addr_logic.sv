137e029d55a12b5b25a801f334e29e55d40a9d02
AhanDatta <ahan.datta312@gmail.com>
2026-03-24 00:17:08 -0500

    Fixing a combinational clock

--- spi_synthesis/addr_logic.sv
+++ spi_synthesis/addr_logic.sv
@@ -19,63 +19,63 @@ module addr_logic (
     logic byte_flag;
     spi_state_t spi_state;
     logic full_rstn;
+    
+    //full async reset
     assign full_rstn = cs && rstn;
 
+    //clock counter
     always_ff @(posedge spi_clk or negedge full_rstn) begin
         if (!full_rstn) begin
             spi_clk_counter <= '0;
         end
         else begin
-            spi_clk_counter <= spi_clk_counter + 1;
+            spi_clk_counter <= spi_clk_counter + 16'd1;
         end
     end 
 
-    //byte flag generator
+    //byte flag generator to signal the end of a byte
     always_comb begin
-        if (!full_rstn) begin
-            byte_flag = '0;
-        end
+        if (spi_clk_counter != 16'd0 && spi_clk_counter[2:0] == 3'b000) begin
+            byte_flag = 1'b1;
+        end 
         else begin
-            if (spi_clk_counter != 0 && spi_clk_counter % 8 == 0) begin
-                byte_flag = 1;
-            end 
-            else begin
-                byte_flag = 0;
-            end
+            byte_flag = 1'b0;
         end
     end
 
-    always_ff @(posedge byte_flag or negedge rstn or negedge cs) begin
-        if (!rstn) begin
-            is_write <= 0;
+    //read state machine for spi
+    always_ff @(posedge spi_clk or negedge full_rstn) begin
+        if (!full_rstn) begin
+            is_write <= 1'b0;
             addr <= '0;
             wdata <= '0;
             spi_state <= SPI_ADDR;
         end
-        else if (!cs) begin
-            //async cs deassertion resets state
-            spi_state <= SPI_ADDR;
-            addr <= '0;
-            is_write <= 0;
-            //keep wdata to avoid writing glitches
-        end
         else begin
-            //process on byte_flag when CS is active
-            case (spi_state)
-                SPI_ADDR: begin
-                    //first byte sets address, is_write
-                    is_write <= byte_deser[7];
-                    addr <= byte_deser[6:0];
-                    wdata <= '0;
-                    spi_state <= SPI_DATA;
-                end
-                
-                SPI_DATA: begin
-                    wdata <= byte_deser[7:0];
-                    addr <= addr + 1; //supports address rollover
-                    spi_state <= SPI_DATA; //stay in SPI_DATA for additional bytes until cs
-                end
-            endcase
+            // Synchronous Clock Enable
+            if (byte_flag) begin
+                case (spi_state)
+                    SPI_ADDR: begin
+                        //first byte sets address, is_write
+                        is_write <= byte_deser[7];
+                        addr <= byte_deser[6:0];
+                        wdata <= '0;
+                        spi_state <= SPI_DATA;
+                    end
+                    
+                    SPI_DATA: begin
+                        wdata <= byte_deser[7:0];
+                        addr <= addr + 7'd1; //supports address rollover
+                        
+                        // Remains in SPI_DATA state to accept subsequent bytes
+                        spi_state <= SPI_DATA; 
+                    end
+                    
+                    default: begin
+                        spi_state <= SPI_ADDR;
+                    end
+                endcase
+            end
         end
     end
 

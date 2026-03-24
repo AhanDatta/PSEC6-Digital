6cacf7f1923fd464efebdf6a2a86b80d0b0b244e
AhanDatta <ahan.datta312@gmail.com>
2026-03-23 23:51:37 -0500

    Making readout mux synthesizable

--- spi_synthesis/readout_mux.sv
+++ spi_synthesis/readout_mux.sv
@@ -1,14 +1,14 @@
 module readout_mux #(
-    parameter integer CH_REG_START_ADDR = 12, //first channel-based reg address
-    parameter integer CH_REG_STOP_ADDR = 67, //last channel-based reg address
-    parameter integer NUM_REGS_PER_CH = 7 //there are 7 register for each channel
+    parameter integer CH_REG_START_ADDR = 12, 
+    parameter integer CH_REG_STOP_ADDR = 67,
+    parameter integer NUM_REGS_PER_CH = 7
 ) (
-    input logic spi_clk, //used to track addr one cycle back
-    input logic [7:0] poci_ch, //timestamps from channels
-    input logic poci_spi, //from SPI itself
+    input logic spi_clk, 
+    input logic [7:0] poci_ch, 
+    input logic poci_spi, 
     input logic cs,
     input logic rstn,
-    input logic [6:0] addr, //used to control the mux
+    input logic [6:0] addr, 
 
     output logic poci
 );
@@ -17,6 +17,8 @@ module readout_mux #(
     logic [6:0] addr_one;
 
     assign full_rstn = cs & rstn;
+
+    //delays the address by one to match the switching time properly
     always_ff @(posedge spi_clk or negedge full_rstn) begin
         if (!full_rstn) begin
             addr_one <= '0;
@@ -26,12 +28,20 @@ module readout_mux #(
         end
     end
 
-    //POCI Output Multiplexer
+    //explicit mux for the poci output
     always_comb begin
-        if (addr_one == '0) poci = 0;
-        else if (addr_one < CH_REG_START_ADDR) poci = poci_spi;
-        else if (addr_one <= CH_REG_STOP_ADDR) poci = poci_ch[((addr_one - CH_REG_START_ADDR)/NUM_REGS_PER_CH)]; //floor is implicit, choses correct channel
-        else poci = 0;
+        case (addr_one) inside
+            7'd0:            poci = 1'b0;
+            [7'd12 : 7'd18]: poci = poci_ch[0];
+            [7'd19 : 7'd25]: poci = poci_ch[1];
+            [7'd26 : 7'd32]: poci = poci_ch[2];
+            [7'd33 : 7'd39]: poci = poci_ch[3];
+            [7'd40 : 7'd46]: poci = poci_ch[4];
+            [7'd47 : 7'd53]: poci = poci_ch[5];
+            [7'd54 : 7'd60]: poci = poci_ch[6];
+            [7'd61 : 7'd67]: poci = poci_ch[7];
+            default:         poci = poci_spi;
+        endcase
     end
 
 endmodule
